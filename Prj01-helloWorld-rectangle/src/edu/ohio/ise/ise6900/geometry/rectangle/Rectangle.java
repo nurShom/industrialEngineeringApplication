55ed08045a5192c7fa8ae0980011622c8f0df134
shomik <arafatnur01@gmail.com>
2017-01-14 13:54:01 -0500

    rectangle code completed.

--- Prj01-helloWorld-rectangle/src/edu/ohio/ise/ise6900/geometry/rectangle/Rectangle.java
+++ Prj01-helloWorld-rectangle/src/edu/ohio/ise/ise6900/geometry/rectangle/Rectangle.java
@@ -1,22 +1,89 @@
 package edu.ohio.ise.ise6900.geometry.rectangle;
 
+import java.util.Scanner;
+
 public class Rectangle {
 	
-	private int width, height;
+	private double width, height;
 	
+	protected double getArea(){
+		return this.width * this.height;
+	}
+	
+	protected double getPerimiter(){
+		return 2 * (this.width + this.height);
+	}
+	
+	protected double getDiagonal(){
+		return Math.sqrt(Math.pow(width, 2) + Math.pow(height, 2));
+	}
+	
+	protected double getAreaOfCircumscribedCircle(){
+		return Math.PI * Math.pow((this.getDiagonal()/2), 2);
+	}
 
 	public static void main(String[] args) {
 		
-		System.out.println("Please enter height and width.\n Height:");
+		Rectangle rec = new Rectangle();
+		Scanner sin = new Scanner(System.in);
+		System.out.print("Please enter height and width.\n Height: ");
+		rec.setHeight(Double.parseDouble(sin.nextLine()));
+		System.out.print(" Width: ");
+		rec.setWidth(Integer.parseInt(sin.nextLine()));
+		sin.close();
 		
+		System.out.println(rec.toString());
+		System.out.println("Area of the rectangle: " + rec.getArea());
+		System.out.println("Perimeter of the rectangle: " + rec.getPerimiter());
+		System.out.println("Length of diagonal of the rectangle: " + rec.getDiagonal());
+		System.out.println("Area of the circumscribed circle of the rectangle: " + rec.getAreaOfCircumscribedCircle());
+		
+	}
 
+
+	@Override
+	public String toString() {
+		return "Rectangle [width=" + width + ", height=" + height + "]";
+	}
+
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = (int) (prime * result + height);
+		result = (int) (prime * result + width);
+		return result;
+	}
+
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Rectangle other = (Rectangle) obj;
+		if (height != other.height)
+			return false;
+		if (width != other.width)
+			return false;
+		return true;
 	}
 
 
 	/**
 	 * @return the width
 	 */
-	public int getWidth() {
+	public double getWidth() {
 		return width;
 	}
 
@@ -24,7 +91,7 @@ public class Rectangle {
 	/**
 	 * @param width the width to set
 	 */
-	public void setWidth(int width) {
+	public void setWidth(double width) {
 		this.width = width;
 	}
 
@@ -32,7 +99,7 @@ public class Rectangle {
 	/**
 	 * @return the height
 	 */
-	public int getHeight() {
+	public double getHeight() {
 		return height;
 	}
 
@@ -40,7 +107,7 @@ public class Rectangle {
 	/**
 	 * @param height the height to set
 	 */
-	public void setHeight(int height) {
+	public void setHeight(double height) {
 		this.height = height;
 	}
 
