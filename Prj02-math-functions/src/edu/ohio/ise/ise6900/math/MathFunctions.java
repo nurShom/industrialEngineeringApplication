a5870ac36bf5c5fcc99cd2fda052e0613518abb8
Nur Shomik <arafatnur01@gmail.com>
2017-01-20 19:33:08 -0500

    Project 02 sin and asin methods added.

--- Prj02-math-functions/src/edu/ohio/ise/ise6900/math/MathFunctions.java
+++ Prj02-math-functions/src/edu/ohio/ise/ise6900/math/MathFunctions.java
@@ -17,7 +17,16 @@ import java.util.Scanner;
  */
 public class MathFunctions {
 	
-	protected final int SIN = 1,
+	public static final String ANSI_RESET = "\u001B[0m",
+			ANSI_BLACK = "\u001B[30m",
+			ANSI_RED = "\u001B[31m",//"\u001B31;1m"
+			ANSI_GREEN = "\u001B[32m",
+			ANSI_YELLOW = "\u001B[33m",
+			ANSI_BLUE = "\u001B[34m",
+			ANSI_PURPLE = "\u001B[35m",
+			ANSI_CYAN = "\u001B[36m",
+			ANSI_WHITE = "\u001B[37m";
+	protected static final int SIN = 1,
 			COS = 2,
 			TAN = 3,
 			ASIN = 4,
@@ -27,9 +36,10 @@ public class MathFunctions {
 			SQRT = 8,
 			POW = 9,
 			FACT = 10;
-
+	private String errMsg, outMsg;
 	/**
 	 * @param args
+	 * @throws InterruptedException 
 	 */
 	public static void main(String[] args) {
 
@@ -44,19 +54,31 @@ public class MathFunctions {
 			option = scan.nextLine();
 
 			if (option != null && (option.equalsIgnoreCase("exit") || option.equalsIgnoreCase("quit"))) {
+				System.out.println("\nThank you for using MathFunctions!");
 				break;
 			}
 			try{
 				choice = Integer.parseInt(option);				
-				System.out.print("Enter the number you want to calculate:");
+				System.out.print("  Enter input:");
 				input = Double.parseDouble(scan.nextLine());
 			} catch (NumberFormatException nfe) {
 //				nfe.printStackTrace();
-				System.err.println("\n  Please enter a valid number!");
+				mf.setErrMsg("Please enter a valid number");
 				continue;
 			}
 			
-			switch(choice){
+			switch(choice) {
+			case SIN:
+				mf.setOutMsg("sin(" +input + ") = " + mf.getSinResult(input));
+				break;
+			case ASIN:
+				if(mf.isAsinParameterValid(input)){
+					mf.setOutMsg("asin(" +input + ") = " + mf.getAsinResult(input) + " degrees");
+				}
+				else{
+					mf.setErrMsg("Please enter a number between -1.0 and 1.0 for arcsin");
+				}
+				break;
 				
 			}
 
@@ -64,10 +86,26 @@ public class MathFunctions {
 		scan.close();
 
 	}
+
+	private double getSinResult(double input) {
+		return Math.sin(Math.toRadians(input));
+	}
 	
+	private double getAsinResult(double input) {
+		return Math.toDegrees((Math.asin(input)));
+	}
+
+	private boolean isAsinParameterValid(double input) {
+		if(input>=-1 && input<=1){
+			return true;
+		}
+		return false;
+	}
+
 	protected void promptUser(){
 		System.out.println(
-				"\n  Selection of any of the math functions is required to proceed."
+				"\n  ----------------------------------------------"
+//				+ "\n  Selection of any of the math functions is required to proceed."
 				+ "\n  (Enter 'exit' or 'quit' to exit the program)\n"
 				+ "\n  Enter any of the following numbers:" 
 				+ "\n  ->sin      :  1"
@@ -80,7 +118,60 @@ public class MathFunctions {
 				+ "\n  ->sqrt     :  8" 
 				+ "\n  ->pow      :  9" 
 				+ "\n  ->factorial: 10\n");
+		
+		if(this.getErrMsg() != null){
+			System.out.flush();
+			System.err.flush();
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			System.err.println(" !! " + this.getErrMsg() + " !!\n");
+			System.out.flush();
+			System.err.flush();
+			this.setErrMsg(null);
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		if(this.getOutMsg() != null){
+			System.out.println(" LAST RESULT:\n"
+					+ " ##> " + this.getOutMsg() + "\n");
+		}
+		
 		System.out.print("  $$ select ->");
+	}
+
+	/**
+	 * @return the errMsg
+	 */
+	public String getErrMsg() {
+		return errMsg;
+	}
+
+	/**
+	 * @param errMsg the errMsg to set
+	 */
+	public void setErrMsg(String errMsg) {
+		this.errMsg = errMsg;
+	}
+
+	/**
+	 * @return the outMsg
+	 */
+	public String getOutMsg() {
+		return outMsg;
+	}
+
+	/**
+	 * @param outMsg the outMsg to set
+	 */
+	public void setOutMsg(String outMsg) {
+		this.outMsg = outMsg;
 	}
 
 }
