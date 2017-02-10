20fe758d98f07ac6a2499c5c09bc57a6270e8634
Nur Shomik <arafatnur01@gmail.com>
2017-02-10 14:23:27 -0500

    added exception classes

--- Prj03-manufacturing-system/src/edu/ohio/ise/ise6900/MfgSystem/model/MfgSystem.java
+++ Prj03-manufacturing-system/src/edu/ohio/ise/ise6900/MfgSystem/model/MfgSystem.java
@@ -1,5 +1,6 @@
 package edu.ohio.ise.ise6900.MfgSystem.model;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class MfgSystem extends MfgObject
@@ -9,6 +10,12 @@ public class MfgSystem extends MfgObject
 
 	public MfgSystem(String name){
 		super(name);
+		this.jobs = new HashMap<String, Job>();
+		this.machines = new HashMap<String, Machine>();
+	}
+	
+	public void addJob(Job j) {
+		
 	}
 
 }
